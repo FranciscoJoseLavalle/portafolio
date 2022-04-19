bad0a8d69d5754ac20c5d16842e3e3a5c59da049
Francisco José Lavalle <franlavalle@hotmail.com>
2022-04-19 19:41:34 -0300

    Actualizaciones generales

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,70 @@
 // Variables
 const contenedor = document.querySelector('.main__proyectos-listado');
-const btnVisible = document.querySelector('.btnVisible');
-const btnOculto = document.querySelector('.btnOculto');
-const proyectosOcultos = document.querySelector('.main__proyectos-listadoOculto')
+const boton = document.querySelector('.btnVisible');
 
 // Array de proyectos
+let proyectosMostrados = [];
+
 const proyectos = [
-    { nombre: 'Blog de cubos', lenguajes: 'HTML5, CSS3, SASS, GIT, Bootstrap5 y JavaScript', enlace: 'https://cubosveloces.netlify.app/' },
-    { nombre: 'Worlde', lenguajes: 'HTML5, CSS3 y JavaScript', enlace: 'https://franacho.github.io/wordlefran/' }
+    { nombre: 'Finanzas', lenguajes: 'HTML5, CSS3, GIT, JavaScript', enlace: 'https://finanzaspersonales.netlify.app/', imagen: './img/finanzas.webp'},
+    { nombre: 'Blog de cubos', lenguajes: 'HTML5, CSS3, SASS, GIT, Bootstrap5 y JavaScript', enlace: 'https://cubosveloces.netlify.app/', imagen: './img/cubo.webp' }
+];
+
+const proyectitosOcultos = [
+    { nombre: 'Wordle', lenguajes: 'HTML5, CSS3 y JavaScript', enlace: 'https://franciscojoselavalle.github.io/wordlefran/', imagen: './img/wordle.webp' },
+    { nombre: 'Blog de café', lenguajes: 'Incluye HTML5 y CSS3', enlace: 'https://cafeblogterminado.netlify.app/', imagen: './img/cafe.webp'}
 ];
 
 // Eventos
 
 // Evento para mostrar los proyectos ocultos
-btnVisible.addEventListener('click', () => {
-    btnVisible.classList.add('btnVisibleOculto');
-    btnOculto.classList.add('btnOcultoVisible');
-    proyectosOcultos.classList.remove('main__proyectos-listadoOculto');
+document.addEventListener('DOMContentLoaded', () => {
+    proyectosMostrados = [...proyectos];
+    escribirHTML();
+})
+
+boton.addEventListener('click', () => {
+    mostrarProyectos();
 });
 
-// Evento para ocultar los proyectos
-btnOculto.addEventListener('click', () => {
-    btnVisible.classList.remove('btnVisibleOculto');
-    btnOculto.classList.remove('btnOcultoVisible');
-    proyectosOcultos.classList.add('main__proyectos-listadoOculto');
+function escribirHTML() {
+    contenedor.textContent = '';
+    proyectosMostrados.forEach(element => {
+        // Crear los elementos
+        const div = document.createElement('div');
+        const separador = document.createElement('div');
+        const img = document.createElement('img');
+        const button = document.createElement('button');
+        const a = document.createElement('a');
+        const p = document.createElement('p');
+        
+        // Asignar propiedades
+        div.classList.add('main__proyectos-cont');
+        div.setAttribute('data-aos', "fade-up");
+        div.append(separador);
+        img.src = element.imagen;
+        separador.append(img);
+        button.classList.add('boton', 'botonProyectos');
+        separador.append(button);
+        a.textContent = element.nombre;
+        a.href = element.enlace;
+        a.target = '_blank'
+        button.append(a);
+        p.textContent = element.lenguajes;
+        div.append(p);
 
-});
-
-// Bucle para crear los proyectos
-for (let proyecto in proyectos) {
-    const proyectosNombre = proyectos[proyecto].nombre;
-
-    const div = document.createElement('div');
-
-    for (let i = 0; i < proyectos.length; i++) {
-        div.innerHTML = `
-        <div class="main__proyectos-cont" data-aos="fade-up">
-            <div>
-            <img src="./img/code.png" alt="Icono Proyecto">
-            <button class="boton botonProyectos"><a href="${proyectos[proyecto].enlace}" target="_blank">${proyectosNombre}</a></button>
-            </div>
-            <p>Incluye ${proyectos[proyecto].lenguajes}</p>
-        </div>`;
+        // Asignarlos al contenedor
         contenedor.append(div);
-    };
+    })
 };
+
+function mostrarProyectos() {
+    if (proyectosMostrados.length == 2) {
+        proyectosMostrados = [...proyectos, ...proyectitosOcultos];
+        boton.textContent = 'Ver menos';
+    } else if (proyectosMostrados.length != 2) {
+        proyectosMostrados = [...proyectos];
+        boton.textContent = 'Ver más'
+    }
+    escribirHTML();
+}
